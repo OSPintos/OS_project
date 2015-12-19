731e04939ac1ca82bb8d11145d2f81ce21b8533b
alfredsamy <alfred_samy2010@yahoo.com>
2015-12-19 23:03:09 +0200

    Argument Passing

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -1,20 +1,56 @@
-#include "userprog/syscall.h"
 #include <stdio.h>
 #include <syscall-nr.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include "userprog/syscall.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "devices/shutdown.h"
+#include "threads/vaddr.h"
+#include "threads/malloc.h"
+#include "filesys/file.h"
 
-static void syscall_handler (struct intr_frame *);
-
-void
-syscall_init (void) 
-{
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+static void syscall_handler(struct intr_frame *);
+void syscall_init(void) {
+	intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
-{
-  printf ("system call!\n");
-  thread_exit ();
+static void syscall_handler(struct intr_frame *f) {
+	int syscall_num = *(int *) (f->esp); //get the syscall_num from the stack
+	if (syscall_num == SYS_WRITE) {
+		printf("Write\n");
+	}
+	else if (syscall_num == SYS_HALT)
+		syscall_halt();
+	else if (syscall_num == SYS_EXIT) {
+		syscall_exit(*(int *) (f->esp + 4));
+	}
+	return;
+}
+
+/* Reads a byte at user virtual address UADDR.
+ UADDR must be below PHYS_BASE.
+ Returns the byte value if successful, -1 if a segfault
+ occurred. */
+static int get_user(const int *uaddr) {
+	if ((void *) uaddr >= PHYS_BASE) {
+		printf("EXIT");
+		syscall_exit(-1);
+	}
+	int result;
+	asm ("movl $1f, %0; movzbl %1, %0; 1:"
+			: "=&a" (result) : "m" (*uaddr));
+	return result;
+}
+int syscall_write(int fd, void *buffer, unsigned size) {
+
+	return -1;
+}
+void syscall_halt(void) {
+	shutdown_power_off();
+}
+void syscall_exit(int status) {
+	printf("%s: exit(%d)\n", thread_current()->name, status);
+	thread_exit();
 }
